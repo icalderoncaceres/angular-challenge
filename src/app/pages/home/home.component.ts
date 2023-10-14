5b0462fa481b0134700070cd28722abf8543137b
Ivan Calderon <zoomtechnology8@gmail.com>
2023-10-14 13:20:13 -0500

    fix(performance): implements async pipe and trackby for better performance

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IUser } from 'src/app/common/models/userModel.model';
 import { UserService } from 'src/app/common/services/user/user.service';
 import { AuthService } from 'src/app/common/services/auth/auth.service';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -11,19 +12,23 @@ import { AuthService } from 'src/app/common/services/auth/auth.service';
 export class HomeComponent implements OnInit {
   list: IUser[] = [];
   currentUser: string = '';
+  users$: Observable<IUser[]> = of([]);
   constructor(
     private userService: UserService,
     private authService: AuthService
   ) {}
 
   ngOnInit(): void {
-    this.userService.get()
-      .subscribe((users: IUser[]) => this.list = users);
+    this.users$ = this.userService.get();
     
     this.currentUser = this.authService.get();
   }
 
   signUp() {
     this.authService.signUp();
+  }
+
+  userById(index: number, user: IUser) {
+    return user.id
   }
 }
